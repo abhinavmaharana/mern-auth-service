506a4e856b29978104082b03eb9df807a8e4e116
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-09-15 17:27:08 +0530

    ✅ Refactoring error and logger handling

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,6 +1,8 @@
 import { Repository } from 'typeorm';
 import { User } from '../entity/User';
 import { UserData } from '../types';
+import createHttpError from 'http-errors';
+import logger from '../config/logger';
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
@@ -10,15 +12,24 @@ export class UserService {
         email,
         password,
     }: UserData): Promise<User> {
-        // Create a new user instance
-        const user = this.userRepository.create({
-            firstName,
-            lastName,
-            email,
-            password,
-        });
+        try {
+            // Create a new user instance
+            const user = this.userRepository.create({
+                firstName,
+                lastName,
+                email,
+                password,
+            });
 
-        // Save the user to the database
-        return await this.userRepository.save(user);
+            // Save the user to the database
+            return await this.userRepository.save(user);
+        } catch (err) {
+            logger.error(err);
+            const error = createHttpError(
+                500,
+                'Failed to store the data in the database',
+            );
+            throw error;
+        }
     }
 }
